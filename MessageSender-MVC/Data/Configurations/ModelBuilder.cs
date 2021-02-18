e616a2668a663ec6ddaa17c78eb1c28896bd886d
Mohammad Sayyed <mohamed.sayed@elzatona.com>
2021-02-19 01:28:13 +0200

    - add domain entities - add entities configuration - create message sender db

---
+++ MessageSender-MVC/Data/Configurations/ModelBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace MessageSender_MVC.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public partial class MessageSenderDbContext: DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new Configurations.MobileContactConfig())
+                .ApplyConfiguration(new Configurations.MobileMessageConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
