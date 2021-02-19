6cb3bbecb414b0d2162b92b23668ded13ebe5c7e
Mohammad Sayyed <mohamed.sayed@elzatona.com>
2021-02-20 01:55:51 +0200

    seed mobile numbers function

--- MessageSender-MVC/Domain/MobileContact.cs
+++ MessageSender-MVC/Domain/MobileContact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,46 @@ namespace MessageSender_MVC.Domain
     public class MobileContact : DomainEntity<int>
     {
         public string MoblieNumber { get; private set; }
+
+
+        public static MobileContact New()
+        {
+            return new MobileContact();
+        }
+        
+        public static List<MobileContact> NewList()
+        {
+            return new List<MobileContact>();
+        }
+        
+        public MobileContact Update(int id, string mobileNumber)
+        {
+            Id = id;
+            MoblieNumber = mobileNumber;
+            return this;
+        }
+
+        public static List<MobileContact> SeedMobileNumbers()
+        {
+            var file = @"D:\Tasks\TA-Telecom\MessageSender-MVC\MobileNumbers.txt";
+
+            if (File.Exists(file))
+            {
+                var contacts = NewList();
+                
+                var lines = File.ReadAllLines(file);
+
+                var id = 0;
+
+                foreach (var line in lines)
+                {
+                    id++;
+                    var contact = New().Update(id, line);
+                    contacts.Add(contact);
+                }
+                return contacts;
+            }
+            return NewList();
+        }
     }
 }
